f8e428b41e6120731ac6c6e695940ebbd753b20f
Diogenesoftoronto <kmannoel@gmail.com>
2022-03-10 22:40:48 -0500

    add new line to text add change append child to append

--- write-and-tell-mvp/src/index.js
+++ write-and-tell-mvp/src/index.js
@@ -29,35 +29,39 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function addElement (text) {
+function addElementToNode (text, className) {
   // create a new div element
   const newParagraph = document.createElement("p");
 
   // and give it some content
-  const newContent = document.createTextNode(text);
+  const newContent = document.createTextNode(text +'\n');
 
   // add the text node to the newly created div
-  newDiv.appendChild(newContent);
+  newParagraph.appendChild(newContent);
 
   // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementsByClassName("card--prompt");
-  document.body.insertaAfter(newParagraph, currentDiv);
+  const currentDiv = document.getElementsByClassName(className);
+  currentDiv.append(newParagraph);
 }
 
 
 const getWritingPrompt = () => {
   const randInt = getRandomInt(getAllWritingPrompt.length);
   const randPrompt = getAllWritingPrompt[randInt];
-  
+  addElementToNode(randPrompt, "card--prompt")
   // return randPrompt;
 }
 const getSimplePrompt = () => {
   const randInt = getRandomInt(getAllSimplePrompt.length);
   const randPrompt = getAllSimplePrompt[randInt];
+  addElementToNode(randPrompt, "card--prompt")
+
   // return randPrompt;
 }
 const getEstablishedUniversePrompt = () => {
   const randInt = getRandomInt(getAllEstablishedUniversePrompt.length);
   const randPrompt = getAllEstablishedUniversePrompt[randInt];
+  addElementToNode(randPrompt, "card--prompt")
+
   // return randPrompt;
 }
