b4d4a01bb40a661089c43a7929e1fe6868b90b3d
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-27 23:58:11 -0400

    full app in express

--- server/main.js
+++ server/main.js
@@ -3,4 +3,9 @@ const e = require("express")()
  function apicall (req, res) {
   res.send('Hello World')
 }
-e.get('http://api.reddit.com', )
+function apilisten () {
+  console.log("listening on port 8080")
+}
+e.get('/', apicall)
+
+e.listen(8080, apilisten)
