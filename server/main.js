a88fd8a672efd21a230a211b895bb13e2a211a0a
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-28 00:18:30 -0400

    add middle ware function

--- server/main.js
+++ server/main.js
@@ -6,6 +6,11 @@ const e = require("express")()
 function apilisten () {
   console.log("listening on port 8080")
 }
+function apimiddleware (req, res, next) {
+  console.log("weiners are juicy and thick")
+  next()
+}
+
 e.get('/', apicall)
 
 e.listen(8080) //works no call back is required
