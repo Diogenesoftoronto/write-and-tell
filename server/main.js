c569f9bde940a59063f44a4c6682cff83bb20438
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-29 18:42:32 -0400

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,16 +1,16 @@
-const e = require("express")()
+const e = require("express")();
 
- function apicall (req, res) {
-  res.send('Hello World')
+function apicall(req, res) {
+  res.send("Hello World");
 }
-function apilisten () {
-  console.log("listening on port 8080")
+function apilisten() {
+  console.log("listening on port 8080");
 }
-function apimiddleware (req, res, next) {
-  console.log("weiners are juicy and thick")
-  next()
+function apimiddleware(req, res, next) {
+  res.json("Hello");
+  next();
 }
-
-e.get('/', apicall)
-
-e.listen(8080) //works no call back is required
+// call middleware before other requests
+// e.use(apimiddleware)
+e.get("/", apicall);
+e.listen(8080); //works no call back is required
